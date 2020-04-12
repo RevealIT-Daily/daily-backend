7b11af299522d18b7cad42ede100fa5734404c84
PedroDBahena <pedrodelonbahena26@gmail.com>
2020-04-12 15:33:37 -0500

    Ecosystem updated

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -22,7 +22,7 @@ module.exports = {
       user : 'root',
       host : '74.208.169.91',
       ref  : 'origin/develop',
-      repo : 'git@github.com:PedroDBahena/daily-backend.git',
+      repo : 'git@github.com:RevealIT-Daily/daily-backend.git',
       path : '/var/www/daily-backend/',
       'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
     }
