431e666c67d889bfa2471ad61a0396b0a296894b
PedroDBahena <pedrodelonbahena26@gmail.com>
2020-04-16 15:12:41 -0500

    Status crud created

--- routes/routes.js
+++ routes/routes.js
@@ -1,11 +1,11 @@
 const { Router } = require('express');
 const router = Router();
 
-router.get('/', function(req, res){
+router.get('/', function (req, res) {
     res.send('Working')
 });
 
-const user = require('../app/controllers/StatusController');
+const status = require('../app/controllers/StatusController');
 
 /***
  * @swagger
@@ -32,7 +32,7 @@ const user = require('../app/controllers/StatusController');
  *          201:
  *              description: Returns status object
  */
-router.post('/api/status', user.create);
+router.post('/api/status', status.create);
 
 /***
  * @swagger
@@ -40,11 +40,65 @@ router.post('/api/status', user.create);
  *  get:
  *     tags:
  *          - Status
- *     description: Use to get all statuses
+ *     description: Use to get all status
  *     responses:
  *          '200':
  *              description: Returns status object
  */
-router.get('/api/status', user.findAll);
+router.get('/api/status', status.findAll);
+
+/***
+ * @swagger
+ * /api/status/{id}:
+ *  get:
+ *     tags:
+ *          - Status
+ *     description: Get status by primary key
+ *     produces:
+ *          - application/json
+ *     parameters:
+ *          - name: id
+ *            description: Primary key
+ *            in: path
+ *     responses:
+ *          '200':
+ *              description: Returns status object
+ */
+router.get('/api/status/:id', status.findById);
+
+/***
+ * @swagger
+ * /api/status/:
+ *  put:
+ *     tags:
+ *          - Status
+ *     description: Update status description by primar key
+ *     produces:
+ *          - application/json
+ *     parameters:
+ *          - name: Status description
+ *            description: Status object
+ *            in: body
+ *            required: true
+ *            schema:
+ *              type: object
+ *              required: 
+ *                  - id
+ *                  - description
+ *              properties:
+ *                  id:
+ *                      type: integer
+ *                  description:
+ *                      type: string
+ *     responses:
+ *          '200':
+ *              description: Returns status object
+ */
+router.put('/api/status', status.update);
+
+/**
+ *  THIS METHOD IS ONLY USED BY BACKEND DEVELOPERS 
+ */
+router.delete('/api/dev/status/:id', status.delete);
 
 module.exports = router;
