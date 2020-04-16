60dd1c795da941db4b77294531ecc3489c0c4e94
PedroDBahena <pedrodelonbahena26@gmail.com>
2020-04-15 21:05:47 -0500

    Swagger implemented

--- app/index.js
+++ app/index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 var cors = require('cors');
 const app = express();
-const path  = require('path');
+const path = require('path');
+
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
 
 // Middlewares //
 app.use(express.json()); // Permit JSON Objects 
@@ -11,10 +15,32 @@ app.use(express.urlencoded({ extended: false })); // Exclude images from request
 app.use(cors());
 
 // PATH //
-app.use(express.static(path.join(__dirname,'../public')));
+app.use(express.static(path.join(__dirname, '../public')));
+
+
+
+// Extended https://swagger.io/speficiation/#infoObject
+const swaggerOptions = {
+    swaggerDefinition: {
+        info: {
+            title: 'Daily API',
+            description: 'API DOCUMENTATION',
+            contact: {
+                name: "GREAT API INFORMATION"
+            },
+            servers: ["http://74.208.169.91:90"]
+        }
+    },
+    apis: ['./routes/*.js']
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use('/api/documentation', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
 
 // Routing //
 app.use(require('../routes/routes'));
+
 
 // set port, listen for requests
 const PORT = process.env.PORT || 3000;
