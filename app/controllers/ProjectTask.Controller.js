429a59034ebcdcadff5a88c6f4df255868ae3cd8
MarcoGarciaV <marco.antonio.gv98@gmail.com>
2020-05-07 22:09:24 -0500

    user crud

--- app/controllers/ProjectTask.Controller.js
+++ app/controllers/ProjectTask.Controller.js
@@ -12,6 +12,7 @@ exports.create = async (req, res) => {
     const description = req.body.description;
     const projectId = req.body.project_id
     const statusId = req.body.status_id;
+    const user_assigned = req.body.user_assigned;
 
 
     if (!name || !description || !projectId ) res.status(400).send({ message: "Please, send all params value" });
@@ -20,6 +21,7 @@ exports.create = async (req, res) => {
         name: name,
         description: description,
         projects_id: projectId,
+        user_assigned:user_assigned,
         status_id:statusId
     }
 
@@ -45,3 +47,50 @@ exports.findAll = async (req, res) => {
             });
         });
 }
+
+exports.findById = async (req, res) => {
+
+    if (!req.params.id) return res.status(400).send({ message: "Id can not be null" });
+
+    await PROJECTASK.findByPk(req.params.id)
+        .then(data => {
+            if (!data) return res.status(404).send({ message: 'Project task not found!' });
+            res.status(200).send({ data: data });
+        }).catch(err => {
+            res.status(400).send({
+                message: err.message || "Something wrong from get project task by id"
+            });
+        });
+}
+
+exports.update = async (req, res) => {
+
+    const id = req.body.id;
+    const name = req.body.name;
+    const description = req.body.description;
+    const status_id= req.body.status_id;
+    const user_assigned = req.body.user_assigned;
+
+    if (!id) res.send(400).send({ message: 'Id can not be null' });
+
+    const projectask = {
+        name: name,
+        description: description,
+        status_id: status_id,
+        user_assigned:user_assigned,
+        finishDate: new Date()
+    }
+
+    await PROJECTASK.findByPk(id)
+        .then(data => {
+            if (!data) return res.status(404).send({ message: 'Project task not found!' });
+
+            if (PROJECTASK.update(projectask, { where: { id: id } }))
+                res.status(200).send({ message: "Project task updated!" });
+        }).catch(err => {
+            res.status(400).send({
+                message: err.message || "Something wrong from update project task"
+            });
+        });
+}
+
