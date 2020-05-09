6a7de62bfead083c2d4b0320354f2b0c010884f7
RevealIT Corporation <revealitcorporation@gmail.com>
2020-05-08 19:31:54 -0500

    Testing autodeploy

--- routes/routes.js
+++ routes/routes.js
@@ -17,7 +17,7 @@ const User = require('../app/controllers/UserController');
  * /api/status:
  *  post:
  *     tags:
- *          - Statusss
+ *          - Status
  *     description: Create new status
  *     produces:
  *          - application/json
